eda004255d31e5060e5875326bda5dbba226aac4
Aminul667 <rahathssn@gmail.com>
2023-05-14 16:40:35 +0600

    70-6 Use Query parameter to load user specific data server

--- index.js
+++ index.js
@@ -42,21 +42,32 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, service_id: 1, price:1 },
+        projection: { title: 1, service_id: 1, price: 1, img: 1 },
       };
 
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
 
-
     // bookings
-    app.post('/bookings', async(req, res) => {
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query);
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
